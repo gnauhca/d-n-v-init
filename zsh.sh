acfcc6dd6e056bded2819c0ea6b1138af9ef8613
Chaz <10112498+gnauhca@users.noreply.github.com>
2022-07-07 19:25:25 +0800

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -1,1 +1,2 @@
+# !/bin/sh
 sudo apt-get update;apt-get install zsh -y;chsh -s /bin/zsh;apt-get install git -y;apt-get install curl;yes | sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";apt-get install autojump -y;echo '. /usr/share/autojump/autojump.sh' >> ~/.zshrc;source .zshrc;
