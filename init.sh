cabbafab47d9c0405ca6b7aa8ec2cf320872560c
Chaz <10112498+gnauhca@users.noreply.github.com>
2022-07-08 23:34:51 +0800

    update

--- init.sh
+++ init.sh
@@ -155,6 +155,7 @@ EOF
 systemctl restart nginx
 systemctl restart v2ray
 
+# BBR 加速
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 sysctl -p # 保存，使更改生效
