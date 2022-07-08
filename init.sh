f9df49d07adfe702a4390b7c69ba94a3ecf3ae2c
Chaz <10112498+gnauhca@users.noreply.github.com>
2022-07-08 23:34:16 +0800

    update

--- init.sh
+++ init.sh
@@ -155,4 +155,11 @@ EOF
 systemctl restart nginx
 systemctl restart v2ray
 
-echo "congratulation! config successfully"
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p # 保存，使更改生效
+sysctl net.ipv4.tcp_available_congestion_control # 查看内核是否已开启BBR
+lsmod | grep bbr # 查看BBR是否启动
+
+
+echo "congratulation! config successfully, please reboot"
